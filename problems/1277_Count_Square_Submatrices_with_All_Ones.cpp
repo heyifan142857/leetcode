dd63097a0668f93eebc00083d7f8f7af300902a6
何一帆 <2473667757@qq.com>
2025-08-20 22:18:38 +0800

    1277

---
+++ problems/1277_Count_Square_Submatrices_with_All_Ones.cpp
@@ -0,0 +1,34 @@
+//
+// Created by user on 2025/8/20.
+//
+
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        int total = 0;
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if(matrix[i][j] == 1){
+                    if(i == 0 || j == 0){
+                        dp[i][j] = 1;
+                    }else{
+                        dp[i][j] = min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1])) + 1;
+                    }
+                    total += dp[i][j];
+                }else{
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return total;
+    }
+};
