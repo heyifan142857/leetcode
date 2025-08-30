76bac4b9004be4aaa246528ff90ff8a7c0e84bd1
何一帆 <2473667757@qq.com>
2025-08-30 21:06:47 +0800

    189

---
+++ problems/189_Rotate_Array.cpp
@@ -0,0 +1,18 @@
+//
+// Created by user on 2025/8/30.
+//
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> newArr(n);
+        for (int i = 0; i < n; ++i) {
+            newArr[(i + k) % n] = nums[i];
+        }
+        nums.assign(newArr.begin(), newArr.end());
+    }
+};
