6365045e7c0d82a80c6cdb3edd04181faa55b0bf
何一帆 <2473667757@qq.com>
2025-09-04 09:14:48 +0800

    3516

---
+++ problems/3516_Find_Closest_Person.cpp
@@ -0,0 +1,21 @@
+//
+// Created by user on 2025/9/4.
+//
+
+#include <cmath>
+
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        int d1 = abs(x-z);
+        int d2 = abs(y-z);
+
+        if(d1 > d2){
+            return 2;
+        }else if(d1 < d2){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+};
