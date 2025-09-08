29d30fbd586aae1865c2b2d77d2adc03cca9fd12
何一帆 <2473667757@qq.com>
2025-09-08 21:41:21 +0800

    994

---
+++ problems/994_Rotting_Oranges.cpp
@@ -0,0 +1,45 @@
+//
+// Created by user on 2025/9/8.
+//
+
+#include <vector>
+#include <queue>
+#include <climits>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        queue<pair<int,int>> q;
+        int fresh = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2) q.push({i, j});
+                else if (grid[i][j] == 1) fresh++;
+            }
+        }
+
+        int minutes = -1;
+        vector<int> dx = {-1, 1, 0, 0};
+        vector<int> dy = {0, 0, -1, 1};
+
+        while (!q.empty()) {
+            int size = q.size();
+            minutes++;
+            for (int i = 0; i < size; i++) {
+                auto [x, y] = q.front(); q.pop();
+                for (int d = 0; d < 4; d++) {
+                    int nx = x + dx[d], ny = y + dy[d];
+                    if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == 1) {
+                        grid[nx][ny] = 2;
+                        fresh--;
+                        q.push({nx, ny});
+                    }
+                }
+            }
+        }
+        return fresh == 0 ? max(0, minutes) : -1;
+    }
+};
