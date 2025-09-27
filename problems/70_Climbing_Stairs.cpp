b14e451b3aba73547b63ffc2dc50c3b630d4bcef
何一帆 <2473667757@qq.com>
2025-09-27 15:07:18 +0800

    70

---
+++ problems/70_Climbing_Stairs.cpp
@@ -0,0 +1,20 @@
+//
+// Created by user on 2025/9/27.
+//
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1, 0);
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+
+        return dp[n];
+    }
+};
