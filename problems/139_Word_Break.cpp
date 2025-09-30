3b7988d05750d96ae29b1a06ea7d106d9cf3d8eb
何一帆 <2473667757@qq.com>
2025-10-01 00:10:03 +0800

    139

---
+++ problems/139_Word_Break.cpp
@@ -0,0 +1,29 @@
+//
+// Created by user on 2025/9/30.
+//
+#include <string>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> set;
+        for (auto word: wordDict) {
+            set.insert(word);
+        }
+        int n = s.size();
+        vector<bool> dp(n+1, false);
+        dp[0] = true;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if(dp[j] && set.contains(s.substr(j, i-j))){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+};
