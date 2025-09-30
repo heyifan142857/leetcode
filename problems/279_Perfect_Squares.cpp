fd241963a96cb6b4cb4be8116f7ea3c404a37586
何一帆 <2473667757@qq.com>
2025-09-30 19:49:44 +0800

    279

---
+++ problems/279_Perfect_Squares.cpp
@@ -0,0 +1,20 @@
+//
+// Created by user on 2025/9/30.
+//
+#include <vector>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> dp(n+1, INT_MAX);
+        dp[0] = 0;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j*j <= i; ++j) {
+                dp[i] = min(dp[i], 1+dp[i-j*j]);
+            }
+        }
+        return dp[n];
+    }
+};
