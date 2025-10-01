5ac025150d899a90820973b1a39cb02c67d6a54b
何一帆 <2473667757@qq.com>
2025-10-01 23:13:33 +0800

    169

---
+++ problems/169_Majority_Element.cpp
@@ -0,0 +1,33 @@
+//
+// Created by user on 2025/10/1.
+//
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return nums[nums.size()/2];
+    }
+};
+
+/**Boyer-Moore 投票算法
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate = -1;
+        int count = 0;
+        for (int num : nums) {
+            if (num == candidate)
+                ++count;
+            else if (--count < 0) {
+                candidate = num;
+                count = 1;
+            }
+        }
+        return candidate;
+    }
+};
+*/
