01804ac1b14a9d347538bdcd6bfb5bbcb32efa65
何一帆 <2473667757@qq.com>
2025-10-01 22:55:57 +0800

    72

---
+++ problems/72_Edit_Distance.cpp
@@ -0,0 +1,36 @@
+//
+// Created by user on 2025/10/1.
+//
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+
+        vector<vector<int>> dp(m+1, vector(n+1, 0));
+
+        for (int i = 0; i <= m; ++i) {
+            dp[i][0] = i;
+        }
+
+        for (int i = 0; i <= n; ++i) {
+            dp[0][i] = i;
+        }
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if(word1[i]==word2[j]){
+                    dp[i+1][j+1] = min(dp[i][j], min(dp[i+1][j], dp[i][j+1])+1);
+                } else{
+                    dp[i+1][j+1] = min(dp[i][j], min(dp[i+1][j], dp[i][j+1]))+1;
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
