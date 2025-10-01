5c96a4ec849753ce1991fe82695331337131f48e
何一帆 <2473667757@qq.com>
2025-10-01 22:15:21 +0800

    1143

---
+++ problems/1143_Longest_Common_Subsequence.cpp
@@ -0,0 +1,28 @@
+//
+// Created by user on 2025/10/1.
+//
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.size();
+        int n = text2.size();
+
+        vector<vector<int>> dp(m+1, vector(n+1, 0));
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if(text1[i]==text2[j]){
+                    dp[i+1][j+1] = dp[i][j]+1;
+                }else{
+                    dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
