f54cb3e01f431b9d3c758436652f5b90ea27a96c
azote <2473667757@qq.com>
2026-03-04 10:00:35 +0800

    1582

---
+++ problems/1582_Special_Positions_in_a_Binary_Matrix.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int numSpecial(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+        vector<int> rowSum(m, 0);
+        vector<int> colSum(n, 0);
+
+        // Calculate row and column sums
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1) {
+                    rowSum[i]++;
+                    colSum[j]++;
+                }
+            }
+        }
+
+        // Count special positions
+        int specialPositions = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1 && rowSum[i] == 1 && colSum[j] == 1) {
+                    specialPositions++;
+                }
+            }
+        }
+
+        return specialPositions;
+    }
+};
