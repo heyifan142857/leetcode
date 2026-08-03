e49fa7baf03856a5dbb0e2e82087ec4cd714e2e9
azote <azote.h2@gmail.com>
2026-08-03 14:49:46 +0800

    1406

---
+++ problems/1406_Stone_Game_III.cpp
@@ -0,0 +1,32 @@
+// 1406. Stone Game III
+// Created automatically
+// Created at 2026-08-03 14:23:47
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string stoneGameIII(vector<int>& stoneValue) {
+        int n = stoneValue.size();
+        vector<int> dp(n + 1, 0);
+
+        for (int i = n - 1; i >= 0; --i) {
+            int take = 0;
+            dp[i] = INT_MIN;
+
+            for (int k = 1; k <= 3 && i + k <= n; ++k) {
+                take += stoneValue[i + k - 1];
+                dp[i] = max(dp[i], take - dp[i + k]);
+            }
+        }
+
+        if (dp[0] > 0) {
+            return "Alice";
+        } else if (dp[0] < 0) {
+            return "Bob";
+        } else {
+            return "Tie";
+        }
+    }
+};
