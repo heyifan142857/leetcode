45f948184b2527bf886abe87a10cd1812d16522c
azote <azote.h2@gmail.com>
2026-08-06 16:31:07 +0800

    3345

---
+++ problems/3345_Smallest_Divisible_Digit_Product_I.cpp
@@ -0,0 +1,27 @@
+// 3345. Smallest Divisible Digit Product I
+// Created automatically
+// Created at 2026-08-06 14:59:22
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  int smallestNumber(int n, int t) {
+    while (true) {
+      int x = n;
+      int product = 1;
+
+      while (x > 0) {
+        product *= x % 10;
+        x /= 10;
+      }
+
+      if (product % t == 0) {
+        return n;
+      }
+
+      n++;
+    }
+  }
+};
