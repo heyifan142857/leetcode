c06e32ee89ef514538994ba54a55448baf5e5b80
azote <azote.h2@gmail.com>
2026-08-11 22:54:38 +0800

    format: 3345

--- problems/3345_Smallest_Divisible_Digit_Product_I.cpp
+++ problems/3345_Smallest_Divisible_Digit_Product_I.cpp
@@ -6,22 +6,22 @@
 using namespace std;
 
 class Solution {
-public:
-  int smallestNumber(int n, int t) {
-    while (true) {
-      int x = n;
-      int product = 1;
+  public:
+    int smallestNumber(int n, int t) {
+        while (true) {
+            int x = n;
+            int product = 1;
 
-      while (x > 0) {
-        product *= x % 10;
-        x /= 10;
-      }
+            while (x > 0) {
+                product *= x % 10;
+                x /= 10;
+            }
 
-      if (product % t == 0) {
-        return n;
-      }
+            if (product % t == 0) {
+                return n;
+            }
 
-      n++;
+            n++;
+        }
     }
-  }
 };
