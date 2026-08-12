47316f39a05e449b3337a407ec95a484b06544cb
azote <azote.h2@gmail.com>
2026-08-12 21:10:38 +0800

    2958

---
+++ problems/2958_Length_of_Longest_Subarray_With_at_Most_K_Frequency.cpp
@@ -0,0 +1,29 @@
+// 2958. Length of Longest Subarray With at Most K Frequency
+// Created automatically
+// Created at 2026-08-12 19:12:47
+
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    int maxSubarrayLength(vector<int> &nums, int k) {
+        unordered_map<int, int> freq;
+        int left = 0;
+        int ans = 0;
+
+        for (int right = 0; right < nums.size(); ++right) {
+            ++freq[nums[right]];
+
+            while (freq[nums[right]] > k) {
+                --freq[nums[left]];
+                ++left;
+            }
+
+            ans = max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+};
