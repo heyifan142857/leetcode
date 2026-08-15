9e1fb04601824cf3a78455f40b9b93d71786bdef
azote <azote.h2@gmail.com>
2026-08-15 23:55:13 +0800

    3702

---
+++ problems/3702_Longest_Subsequence_With_Non_Zero_Bitwise_XOR.cpp
@@ -0,0 +1,24 @@
+// 3702. Longest Subsequence With Non-Zero Bitwise XOR
+
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    int longestSubsequence(vector<int> &nums) {
+        const int n = static_cast<int>(nums.size());
+        int xorSum = 0;
+        bool hasNonZero = false;
+
+        for (int num : nums) {
+            xorSum ^= num;
+            hasNonZero |= num != 0;
+        }
+
+        if (!hasNonZero) {
+            return 0;
+        }
+
+        return xorSum != 0 ? n : n - 1;
+    }
+};
